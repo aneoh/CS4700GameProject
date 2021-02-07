545247626b0334344c50cf401a8d9cb812029779
Inh3ritance <herreralaces@gmail.com>
2021-02-07 12:54:16 -0800

    Cleaned some scripts
    need to match audio with change of player pace

--- 2D-Horror/Assets/Scripts/Player.cs
+++ 2D-Horror/Assets/Scripts/Player.cs
@@ -2,82 +2,65 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
-{
+public class Player : MonoBehaviour {
+
     [SerializeField]
     private int Sanity;
-
-    public float speed = 100;
+    public float speed;
     public Transform obj;
-
     public AudioSource Walking;
     bool Is_Movement;
-    bool Check_Movement = false;
+    bool Is_Running;
+    bool Check_Movement;
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
+    private void OnCollisionEnter2D(Collision2D collision) {
         if (collision.gameObject.tag == "Enemy")
-        {
             Sanity -= 10;
-        }    
-
     }
 
     // Start is called before the first frame update
-    void Start()
-    {
-        Sanity = 100;
-
+    void Start() {
         Walking = GetComponent<AudioSource>();
         Is_Movement = false;
+        Check_Movement = false;
+        Is_Running = false;
+        Sanity = 100;
     }
 
-    public void Update()
-    {
+    public void Update() {
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
-
+        float temp_speed = 0;
+        Is_Running = Input.GetKey(KeyCode.LeftShift) ? true : false;
         Vector3 tempVect = new Vector3(h, v, 0);
+        temp_speed = speed;
+        speed += Is_Running ? 0.5f : 0f;
         tempVect = tempVect.normalized * speed * Time.deltaTime;
-
         obj.transform.position += tempVect;
-        
+        speed = temp_speed;
         // If Movement is found
-        if ((Mathf.Abs(v) > 0.05 ) || (Mathf.Abs(h) > 0.05))
-        {
+        if (Mathf.Abs(v) > 0.05 || Mathf.Abs(h) > 0.05)
             Is_Movement = true;
-        }
         else
-        {
             Is_Movement = false;
-        }
-        Debug.Log(h);
-        Debug.Log(v);
-
     }
+
     // Update is called once per frame
-    void FixedUpdate()
-    {
-        if (Sanity <= 0)
-        {
+    void FixedUpdate() {
+        if (Sanity <= 0) {
             Debug.Log("You're Dead, loser.");
             Sanity = 100;
         }
-        if (Check_Movement == false)
-        {
-            if (Is_Movement)
-            {
+        if (Check_Movement == false) {
+            if (Is_Movement) {
                 Walking.Play();
+                Walking.pitch = Is_Running ? 1.5f : 1f;
                 Check_Movement = true;
-            }
-            else
-            {
+            } else {
                 Walking.Stop();
                 Check_Movement = false;
             }
-        }
-        else if((Check_Movement == true) && (Is_Movement == false))
-        {
+        } else if(Check_Movement && !Is_Movement) {
             Check_Movement = false;
         }
     }
